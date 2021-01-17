77588b9ff4626b2f9e33807cb16bad83f3a7695f
Chris Carlile <christopher.carlile@gmail.com>
2021-01-17 11:57:12 -0800

    new home manager config

--- home.nix
+++ home.nix
@@ -8,12 +8,16 @@
     enable = true;
     userName = "Chris Carlile";
     userEmail = "christopher.carlile@gmail.com";
-    ignores = [ "*.swp" ];
   };
 
   programs.vim = {
     enable = true;
-    plugins = [ "vim-airline" "vim-surround" "vim-repeat" "wal-vim" ];
+    plugins = [ 
+      pkgs.vimPlugins.airline
+      pkgs.vimPlugins.surround
+      pkgs.vimPlugins.repeat
+      pkgs.vimPlugins.nord-vim
+    ];
     settings = {
       ignorecase = true;
       smartcase = true;
@@ -27,10 +31,9 @@
       set autoread
       set encoding=utf8
       set backspace=2
-      set clipboard^=unnamed
+      set clipboard=unnamed
       set laststatus=2
       set noshowmode
-
       imap jk <esc>
       let mapleader = "\<Space>"
       nnoremap ; :
@@ -38,17 +41,49 @@
       nnoremap <Leader>n :bnext<CR>
       nnoremap <Leader>p :bprevious<CR>
       nnoremap <Leader>x :bdelete<CR>
-
       set t_Co=256
-      colorscheme wal
+      colorscheme nord
     '';
   };
 
-  nixpkgs.config.allowUnfree = true;
-
-  xsession.pointerCursor = {
-    package = pkgs.vanilla-dmz;
-    name = "Vanilla-DMZ";
-    size = 128;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "refined";
+      plugins = [ "fzf" ];
+    };
+    initExtra =
+      "
+      export PATH=\"$HOME/bin:$PATH\"
+      fortune | cowsay
+      ";
   };
+
+  programs.fzf = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    enableNixDirenvIntegration = true;
+  };
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "chris";
+  home.homeDirectory = "/Users/chris";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
 }
