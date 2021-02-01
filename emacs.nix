6f949a8ac448e1ee1eff72a5d285f63c9b09eb33
Chris Carlile <christopher.carlile@gmail.com>
2021-01-31 18:19:59 -0800

    Remove posframe, commit to monolithic general bindings

--- emacs.nix
+++ emacs.nix
@@ -62,6 +62,118 @@
             '';
           };
 
+        which-key = {
+          enable = true;
+          config = ''
+            (which-key-mode)
+            (which-key-setup-minibuffer)
+            (setq which-key-sort-order 'which-key-key-order-alpha
+                  which-key-idle-delay 0.01)   
+          '';
+        };
+
+
+        # The dilemma here was - put mode-specific keybindings in
+        # their respective blocks, or leave them in the general.el
+        # monolith? As of right now, the former requires creating a
+        # definer which we refer to in other places in the resulting
+        # lisp code - i.e. not declarative! So I'll do the latter. God
+        # save me.
+
+        general = {
+          enable = true;
+          after = [ "evil" "which-key" ];
+          config = ''
+            (general-evil-setup)
+            (general-nmap
+              ";" 'evil-ex
+              ":" 'evil-repeat-find-char)
+            (general-nmap
+              "C-j" 'evil-window-down
+              "C-k" 'evil-window-up
+              "C-c l" 'winner-redo
+              "C-c h" 'winner-undo
+              "C-]" 'dumb-jump-go
+              "C-'" 'dumb-jump-go
+              "C-t" 'dumb-jump-back
+              "j" 'evil-next-visual-line
+              "k" 'evil-previous-visual-line
+              "C-/" 'evil-avy-goto-char-timer)
+
+            (general-create-definer space-leader-def
+              :prefix "SPC")
+
+            (general-create-definer space-leader-mode-def
+              :prefix "SPC m")
+
+            (space-leader-def
+              :states '(normal visual)
+              :keymaps 'override
+
+              "SPC" '(helm-M-x :which-key "M-x")
+              "g" '(:ignore t :which-key "git")
+              "g s" '(magit-status :which-key "magit")
+              "g r" '(browse-at-remote :which-key "browse on forge")
+              "g b" '(magit-blame :which-key "blame")
+              "g f" '(magit-log-buffer-file "log for file")
+            
+              "b" '(:ignore t :which-key "buffer")
+              "b b" '(helm-mini :which-key "list buffers")
+              "b s" '(switch-to-scratch-buffer :which-key "scratch")
+              "b k" '(kill-buffer :which-key "kill buffer")
+              
+              "f" '(:ignore t :which-key "files")
+              "f f" '(helm-find-files :which-key "find")
+              "f j" '(dired-jump :which-key "dired")
+
+              "h" '(:ignore t :which-key "help")
+              "h k" '(describe-key :which-key "describe key")
+              "h f" '(describe-function :which-key "describe function")
+              "h v" '(describe-variable :which-key "describe variable")
+              "h i" '(info :which-key "info")
+              "h b" '(describe-bindings :which-key "describe bindings")
+              "h m" '(describe-mode :which-key "describe mode")
+
+              "p" '(:ignore t :which-key "project")
+              "p f" '(helm-projectile-find-file :which-key "find file")
+              "p p" '(helm-projectile-switch-project :which-key "switch to project")
+              "p i" '(projectile-invalidate-cache :which-key "invalidate project cache")
+              "l" '(:ignore t :which-key "lisp")
+              "l s" '(eval-last-sexp :which-key "eval last sexp")
+              "l e" '(eval-expression :which-key "eval expression")
+              "l d" '(eval-defun :which-key "eval defun")
+
+              "s" '(:ignore t :which-key "search")
+              "s f" '(helm-do-ag :which-key "search in files")
+              "s p" '(helm-do-ag-project-root :which-key "search in project")
+              "s b" '(helm-do-ag-buffers :which-key "search in buffers")
+              "s s" '(helm-swoop :which-key "swoop")
+              "s m" '(helm-multi-swoop-projectile :which-key "multi swoop")
+
+              "t" '(:ignore t :which-key "toggles/themes")
+              "t t" '(helm-themes :which-key "select theme")
+              "t n" '(global-linum-mode :which-key "toggle line numbers")
+              "t r" '(treemacs :which-key "treemacs")
+
+              "i" '(:ignore t :which-key "inflection")
+              "i k" '(string-inflection-kebab-case :which-key "kebab-case")
+              "i j" '(string-inflection-camelcase :which-key "CamelCase")
+              "i c" '(string-inflection-lower-camelcase :which-key "lowerCamelCase")
+              "i p" '(string-inflection-lower-camelcase :which-key "under_score")
+              
+              "c" '(:ignore t :which-key "comment")
+              "c l" '(comment-region :which-key "comment region")
+              "c u" '(uncomment-region :which-key "uncomment region"))
+
+              "m" '(:ignore t :which-key "major-mode")
+
+            (general-imap "j"
+              (general-key-dispatch 'self-insert-command
+                "k" 'evil-normal-state)
+              "C-w" 'backward-kill-word)
+          '';
+        };
+
         dired = {
           enable = true;
           config = ''
@@ -238,16 +350,6 @@
           '';
         };
 
-        which-key = {
-          enable = true;
-          config = ''
-            (which-key-mode)
-            (which-key-setup-minibuffer)
-            (setq which-key-sort-order 'which-key-key-order-alpha
-                  which-key-idle-delay 0.01)   
-          '';
-        };
-
         helm-themes = {
           enable = true;
         };
@@ -340,13 +442,6 @@
           enable = true;
         };
 
-        helm-posframe = {
-          enable = true;
-          config = ''
-            (helm-posframe-enable)
-          '';
-        };
-
         dap-mode = {
           enable = true;
           hook = [
@@ -389,91 +484,6 @@
           ];
         };
 
-        general = {
-          enable = true;
-          after = [ "evil" "which-key" ];
-          config = ''
-            (general-evil-setup)
-            (general-nmap
-              ";" 'evil-ex
-              ":" 'evil-repeat-find-char)
-            (general-nmap
-              "C-j" 'evil-window-down
-              "C-k" 'evil-window-up
-              "C-c l" 'winner-redo
-              "C-c h" 'winner-undo
-              "C-]" 'dumb-jump-go
-              "C-'" 'dumb-jump-go
-              "C-t" 'dumb-jump-back
-              "j" 'evil-next-visual-line
-              "k" 'evil-previous-visual-line
-              "C-/" 'evil-avy-goto-char-timer)
-
-            (general-nvmap
-              :keymaps 'override
-              :prefix "SPC"
-
-              "SPC" '(helm-M-x :which-key "M-x")
-              "g" '(:ignore t :which-key "git")
-              "g s" '(magit-status :which-key "magit")
-              "g r" '(browse-at-remote :which-key "browse on forge")
-              "g b" '(magit-blame :which-key "blame")
-              "g f" '(magit-log-buffer-file "log for file")
-            
-              "b" '(:ignore t :which-key "buffer")
-              "b b" '(helm-mini :which-key "list buffers")
-              "b s" '(switch-to-scratch-buffer :which-key "scratch")
-              "b k" '(kill-buffer :which-key "kill buffer")
-              
-              "f" '(:ignore t :which-key "files")
-              "f f" '(helm-find-files :which-key "find")
-              "f j" '(dired-jump :which-key "dired")
-
-              "h" '(:ignore t :which-key "help")
-              "h k" '(describe-key :which-key "describe key")
-              "h f" '(describe-function :which-key "describe function")
-              "h v" '(describe-variable :which-key "describe variable")
-              "h i" '(info :which-key "info")
-              "h b" '(describe-bindings :which-key "describe bindings")
-              "h m" '(describe-mode :which-key "describe mode")
-
-              "p" '(:ignore t :which-key "project")
-              "p f" '(helm-projectile-find-file :which-key "find file")
-              "p p" '(helm-projectile-switch-project :which-key "switch to project")
-              "p i" '(projectile-invalidate-cache :which-key "invalidate project cache")
-              "l" '(:ignore t :which-key "lisp")
-              "l s" '(eval-last-sexp :which-key "eval last sexp")
-              "l e" '(eval-expression :which-key "eval expression")
-              "l d" '(eval-defun :which-key "eval defun")
-
-              "s" '(:ignore t :which-key "search")
-              "s f" '(helm-do-ag :which-key "search in files")
-              "s p" '(helm-do-ag-project-root :which-key "search in project")
-              "s b" '(helm-do-ag-buffers :which-key "search in buffers")
-              "s s" '(helm-swoop :which-key "swoop")
-              "s m" '(helm-multi-swoop-projectile :which-key "multi swoop")
-
-              "t" '(:ignore t :which-key "toggles/themes")
-              "t t" '(helm-themes :which-key "select theme")
-              "t n" '(global-linum-mode :which-key "toggle line numbers")
-              "t r" '(treemacs :which-key "treemacs")
-
-              "i" '(:ignore t :which-key "inflection")
-              "i k" '(string-inflection-kebab-case :which-key "kebab-case")
-              "i j" '(string-inflection-camelcase :which-key "CamelCase")
-              "i c" '(string-inflection-lower-camelcase :which-key "lowerCamelCase")
-              "i p" '(string-inflection-lower-camelcase :which-key "under_score")
-              
-              "c" '(:ignore t :which-key "comment")
-              "c l" '(comment-region :which-key "comment region")
-              "c u" '(uncomment-region :which-key "uncomment region"))
-
-            (general-imap "j"
-              (general-key-dispatch 'self-insert-command
-                "k" 'evil-normal-state)
-              "C-w" 'backward-kill-word)
-          '';
-        };
 
         nix-mode = {
           enable = true;
